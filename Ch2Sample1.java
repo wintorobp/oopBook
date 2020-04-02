78f1b568c433ac9069e9a404c196d41c682a3884
wintorobp <wintorobudipuput@darmajaya.ac.id>
2020-04-02 18:47:09 +0700

    menambah file Ch2Sample1.java

---
+++ Ch2Sample1.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Ch2Sample1 {
+    public static void main(String[] args) {
+        JFrame myWindow;
+
+        myWindow = new JFrame();
+        myWindow.setSize(300,200);
+        myWindow.setTitle("my First Java Program");
+        myWindow.setVisible(true);
+    }
+}
